e638713260f2d88e2d147dab2744a9e6107c8f01
celic <cceli@codequarry.net>
2013-10-28 21:00:37 -0400

    Adds more to PP

--- java/src/com/austingulati/opsys/project1/PreemptivePriorityScheduler.java
+++ java/src/com/austingulati/opsys/project1/PreemptivePriorityScheduler.java
@@ -16,6 +16,8 @@ class PreemptivePriorityScheduler extends Scheduler
         // If a process is preempted with a priority less than the workingPriority, it gains full access
         // otherwise it is just put at the end of the RR cycle
 
+        System.out.printf("PROCESSES LEFT: %d %d\n", waitingProcesses.size(), getTime());
+
         Process currentProcess = runningProcesses.get(processor);
         
         // Continue with the current process if it exists
@@ -40,16 +42,15 @@ class PreemptivePriorityScheduler extends Scheduler
         // Sift through priorties in ascending order until a process is found
         for(int currentPriority = workingPriority; currentPriority <= 4; currentPriority++)
         {
-            for(Process nextProcess : waitingProcesses)
+            for(int i = 0; i < waitingProcesses.size(); i++)
             {
-                if(nextProcess.getPriority() == workingPriority)
+                if(waitingProcesses.get(i).getPriority() == workingPriority)
                 {
                     // Set new priority for efficiency
                     workingPriority = currentPriority;
 
                     // Remove found process from waitingProcesses list
-                    int indexToRemove = waitingProcesses.indexOf(nextProcess);
-                    return waitingProcesses.remove(indexToRemove);
+                    return waitingProcesses.remove(i);
                 }
             }
         }
